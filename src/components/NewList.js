efbbe1a988d304f6a200c1cbf0f43b97a1158d1f
chicacode <gerahernan.al@gmail.com>
2022-02-06 17:23:59 +0100

    feat solving issue

--- src/components/NewList.js
+++ src/components/NewList.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import New from './New';
 
-const NewList = ({news})=>(
-    <div className='container'>
+const NewList = ({ news }) => {
+    return (
+        <div className='container'>
+            {news.map((num) =>
+               <New  key={num.url} num={num} />
+            )}
 
-{news.map((num) => <New key={num.url} num={num}/>)};
+        </div>
+    )
+}
 
-    </div>
-);
 
 export default NewList;
